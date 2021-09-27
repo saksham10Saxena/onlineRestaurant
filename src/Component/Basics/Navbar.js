8350c8c6b5c6021a4cd31536ede0ca2f9d34cade
Saksham Saxena <saksham10.saxena@gmail.com>
2021-09-27 15:05:43 +0530

    Online Store without Hooks

---
+++ src/Component/Basics/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Navbar = ({ filterItem, menuList }) => {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="btn-group">
+          {menuList.map((curElem, i) => {
+            return (
+              <button
+                className="btn-group__item"
+                onClick={() => filterItem(curElem)}
+              >
+                {curElem}
+              </button>
+            );
+          })}
+
+          {/* <button className="btn-group__item" onClick={() => setMenuData(Menu)}>
+            All
+          </button> */}
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
